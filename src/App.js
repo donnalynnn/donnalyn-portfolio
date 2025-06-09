4190e9e93bc2b73281b00aa0b5740d5125bcd6bd
donnalynnn <donnalyn.dev@gmail.com>
2025-06-09 22:19:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect, useRef } from 'react';
 import './App.css';
+import ReactMarkdown from 'react-markdown';
 import avatarImage from './assets/avatar.png';
 import avatarImageNight from './assets/avatar-night.png';
 
@@ -10,12 +11,13 @@ import MuteIcon from './assets/Mute.svg';
 import SoundIcon from './assets/Speaker.svg';
 import Night from './assets/Night.svg';
 import Day from './assets/Day.svg';
-import music from './assets/bgmusic.mp3';
-
 
 import speakbrightGif from './assets/speakbright.gif';
 import bizexpense from './assets/bizexpense.gif';
 import rymind from './assets/rymind.mp4';
+import breathewrite from './assets/breathewrite.gif';
+
+
 import resumePDF from './assets/Resume.pdf';
 import FireflyScene from "./FireflyScene.jsx";
 
@@ -84,19 +86,63 @@ function App() {
   const works = [
     {
       title: 'SpeakBright AAC App',
-      // techstack: ![Dart](https://img.shields.io/badge/dart-%230175C2.svg?style=flat&logo=dart&logoColor=white)
-      description: 'An AI-powered AAC mobile app for children with special needs.',
+      techstack: `
+        <img src="https://img.shields.io/badge/Dart-%230175C2.svg?style=flat&logo=dart&logoColor=white" alt="Dart" />
+        <img src="https://img.shields.io/badge/Flutter-%2302569B.svg?style=flat&logo=Flutter&logoColor=white" alt="Flutter" />
+        <img src="https://img.shields.io/badge/Firebase-%23039BE5.svg?style=flat&logo=firebase" alt="Firebase" />
+        <img src="https://img.shields.io/badge/FastAPI-005571?style=flat&logo=fastapi" alt="FastAPI" />
+        <img src="https://img.shields.io/badge/React-%2320232a.svg?style=flat&logo=react&logoColor=%2361DAFB" alt="React" />
+        <img src="https://img.shields.io/badge/Python-3670A0?style=flat&logo=python&logoColor=ffdd54" alt="Python" />
+        <img src="https://img.shields.io/badge/Git-%23F05033.svg?style=flat&logo=git&logoColor=white" alt="Git" />
+      `,
+      description: 'SpeakBright is an Augmentative and Alternative Communication (AAC) app designed to support children with Autism Spectrum Disorder (ASD) and speech impairments. It addresses the limitations of traditional tools like PECS and existing AAC apps by integrating personalized images, Text-to-Speech (TTS), emotion-inclusive communication, and real-time progress tracking.<br/><br/> I contributed to the UI/UX design, the Learn Module, and implemented automatic phase progression based on each child’s proficiency. I also developed the Adaptive Card Recommendation system using Temporal PrefixSpan, a sequential pattern mining algorithm enhanced with time-awareness, to deliver personalized, time-sensitive suggestions based on user behavior.',
       gif: speakbrightGif,
     },
     {
       title: 'BizExpense',
-      description: 'A web app that tracks and visualizes business expenses.',
+      techstack: `
+        <img src="https://img.shields.io/badge/C%23-%23239120.svg?style=flat&logo=c-sharp&logoColor=white" alt="C#" />
+        <img src="https://img.shields.io/badge/.NET-5C2D91?style=flat&logo=.net&logoColor=white" alt=".NET" />
+        <img src="https://img.shields.io/badge/Microsoft%20SQL%20Server-CC2927?style=flat&logo=microsoft%20sql%20server&logoColor=white" alt="MSSql" />
+        <img src="https://img.shields.io/badge/Ngrok-372e66.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/NLP/LLM-316d8c.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/Razor -b37d32.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/TCP Tunneling-983e59.svg?style=flat" alt="badge" />
+
+      `,
+      description: 'Collaborated with a team of 5 to develop an expense tracking web application for Alliance Software Inc. Built a detailed reporting dashboard with category-based filtering and dynamic chart visualization. I integrated an AI-powered chatbot that communicates with a local Microsoft SQL Server via TCP tunneling, enabling real-time expense queries. This innovative integration set our project apart from other web apps and led to our recognition for outstanding performance throughout the training program.',
       gif: bizexpense,
     },
     {
       title: 'RyMind',
-      description: 'A personal portfolio built with React and pixel styling.',
+      techstack: `
+        <img src="https://img.shields.io/badge/Python-3670A0?style=flat&logo=python&logoColor=ffdd54" alt="Python" />
+        <img src="https://img.shields.io/badge/PyTorch-%23EE4C2C.svg?style=flat&logo=PyTorch&logoColor=white" alt="PyTorch" />
+        <img src="https://img.shields.io/badge/Numpy-%23013243.svg?style=flat&logo=numpy&logoColor=white" alt="NumPy" />
+        <img src="https://img.shields.io/badge/OpenCV-%23white.svg?style=flat&logo=opencv&logoColor=white" alt="OpenCV" />
+        <img src="https://img.shields.io/badge/Threading-398475.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/FaceRecognition-6e6dca.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/SpeechRecognition -b37d32.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/NLP-316d8c.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=flat&logo=TensorFlow&logoColor=white" alt="TensorFlow" />
+      `,
+      description: 'RyMind is an AI-powered assistive system designed to support individuals with dementia by helping them recognize loved ones and engage in meaningful conversations. I developed a real-time face recognition module using face_recognition and OpenCV, and built a speech-enabled chatbot powered by a custom intent classification model trained in PyTorch. The system applies natural language processing (NLP) using NLTK and custom utilities, with tokenization, stemming, and Bag-of-Words for intent detection. It supports real-time interaction through speech-to-text and text-to-speech, and intelligently triggers face recognition in response to context-aware queries. This project combines computer vision, machine learning, and natural language.',
       video: rymind,
+    },
+    {
+      title: 'BreatheWrite',
+      techstack: `
+        <img src="https://img.shields.io/badge/Python-3670A0?style=flat&logo=python&logoColor=ffdd54" alt="Python" />
+        <img src="https://img.shields.io/badge/django-%23092E20.svg?style=flat&logo=django&logoColor=white" alt="Django" />
+        <img src="https://img.shields.io/badge/Dart-%230175C2.svg?style=flat&logo=dart&logoColor=white" alt="Dart" />
+        <img src="https://img.shields.io/badge/Flutter-%2302569B.svg?style=flat&logo=Flutter&logoColor=white" alt="Flutter" />
+        <img src="https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=flat&logo=TensorFlow&logoColor=white" alt="TensorFlow" />
+        <img src="https://img.shields.io/badge/Keras-%23D00000.svg?style=flat&logo=Keras&logoColor=white" alt="Keras" />
+        <img src="https://img.shields.io/badge/Emotion Analysis -b37d32.svg?style=flat" alt="badge" />
+        <img src="https://img.shields.io/badge/NLP-316d8c.svg?style=flat" alt="badge" />
+      `,
+      description: 'A journaling app that performs emotion analysis on user journal entries to provide mental health insights during therapy sessions. I implemented the emotion classification model using TensorFlow and Keras, applying NLP techniques to detect emotional states from text. The project includes a Flutter frontend and a Django backend, enabling a seamless, cross-platform user experience.',
+      gif: breathewrite,
     },
   ];
 
@@ -110,7 +156,7 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(true);
 
   useEffect(() => {
-    const audio = new Audio(music);
+    const audio = new Audio(`${process.env.PUBLIC_URL}/bgmusic.mp3`);
     audio.loop = true;
     audio.volume = 0.5;
     audioRef.current = audio;
@@ -190,7 +236,7 @@ function App() {
 
 
         <div className="content-container">
-          <img src={avatarImage} alt="avatar" className="avatar" />
+          <img src={theme === "dark" ? avatarImageNight : avatarImage} alt="avatar" className="avatar" />
           {activeTab === 'about' && (
     <>
           <h1>Donnalyn</h1>
@@ -240,7 +286,12 @@ function App() {
 
             <div className="work-details">
               <h2>{works[currentWorkIndex].title}</h2>
-              <p>{works[currentWorkIndex].description}</p>
+              <div
+                className="work-techstack"
+                dangerouslySetInnerHTML={{ __html: works[currentWorkIndex].techstack }}
+              />
+              <p className='desc' dangerouslySetInnerHTML={{ __html: works[currentWorkIndex].description }} />
+
               <button onClick={goToNextWork} className="carousel-btn next-btn">
                 Next ▶
               </button>
